d314a9037cd382fe5b37d4192e6ebff308f5a2a0
Samuel Iyeh <thesamueliyeh@gmail.com>
2023-03-01 11:02:45 +0000

    Create task 100

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts "#{ARGV[0].scan(/(?<=from:)(\w+|\+?\d+)/).join},#{ARGV[0].scan(/(?<=to:)(\w+|\+?\d+)/).join},#{ARGV[0].scan(/(?<=flags:)(-?\d\:?)+/).join}"
+puts "#{ARGV[0].scan(/(?<=from:)(\w+|\+?\d+)/).join},#{ARGV[0].scan(/(?<=to:)(\w+|\+?\d+)/).join},#{ARGV[0].scan(/(?<=flags:)\S+(?=])/).join}"
